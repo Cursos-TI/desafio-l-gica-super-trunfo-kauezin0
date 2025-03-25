7dd6d09cad3531eb4896976e5e34ccc878b00fcd
Kauê <kauefmfreitas@gmail.com>
2025-03-25 16:43:00 +0000

    comparativo com switch (ainda nao terminado)

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -7,7 +7,7 @@ int main(){
     char estado;
     char codigo_da_carta[10], nome_da_cidade[20];
     unsigned long int populacao;
-    int qnt_ponto_turistico;
+    int qnt_ponto_turistico, comparativo;
     float area, pib, densidade_pop, pib_per_cap, super;
 
 //Mensagem de instrução para o usuário
@@ -61,7 +61,6 @@ int main(){
     float area2, pib2, densidade_pop2, pib_per_cap2, super2;
 
 
-
 //Registro da carta 2
     
     printf("Estado (Uma letra de 'A' a 'H', representando um dos oito estados):\n");
@@ -104,6 +103,39 @@ int main(){
     printf("---------------------\n\nCarta 2\n\n---------------------\n");
     
     printf(" Estado: %c\n Codigo da carta: %s\n Nome da cidade: %s\n Populacao: %ld\n Area: %.2fkm\n PIB: %.2f\n Numeros de pontos turisticos: %d\n Densidade Populacional: %.2f\n PIB Per Capita: %.2f\n Super Poder: %.3f\n---------------------", estado2, codigo_da_carta2, nome_da_cidade2, populacao2, area2, pib2, qnt_ponto_turistico2, densidade_pop2, pib_per_cap2, super2);
+
+// Menu interativo para as escolhas dos atributos
+
+    printf("Escolha o atributo das suas cartas\n");
+    printf("1. Populacao\n");
+    printf("2. Area\n");
+    printf("3. PIB\n");
+    printf("4. Numeros de pontos turisticos\n");
+    printf("5. densidade demografica\n");
+    printf("Escolha: \n");
+    scanf("%d", &comparativo);
+
+    switch (comparativo)
+    {
+    case 1:
+        
+        break;
+    case 2:
+        
+        break;
+    case 3:
+        
+        break;
+    case 4:
+        
+        break;
+    case 5:
+        
+        break;
+    default:
+
+        break;
+    }
 
 //Resultado da comparação das cartas
 //Densidade populacional com o operador NOT
